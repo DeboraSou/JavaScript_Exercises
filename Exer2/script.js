aeb53e4cc1130aa8ef385683b1ec4918e4409f7b
DeboraSou <debora.d3v@gmail.com>
2024-03-04 14:19:18 -0300

    Adiciona programa JavaScript que declara variáveis 'nome' e sobrenome' e atribui valores a elas.

---
+++ Exer2/script.js
@@ -0,0 +1,6 @@
+// Declaração e atribuição de valores nas variáveis 'nome' e 'sobrenome'.
+let nome = "James";
+let sobrenome = "Bond";
+
+// Impressão das informações no console usando template string (delimitadas por crases ``).
+console.log(`Olá o meu nome é ${nome}, ${nome} ${sobrenome}.`);
