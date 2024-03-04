b7775d6d93d5463fe968180c074a1f4501312f99
DeboraSou <debora.d3v@gmail.com>
2024-03-04 14:17:03 -0300

    Adiciona programa JavaScript que declara variáveis 'nome' e 'idade' e atribui valores a elas.

---
+++ Exer1/script.js
@@ -0,0 +1,6 @@
+// Declaração e atribuição de valores nas variáveis 'nome' e 'idade'.
+let nome = "Margot";
+let idade = 34;
+
+// Impressão das informações no console.
+console.log("Olá meu nome é " + nome + ", tenho " + idade + " anos de idade.");
