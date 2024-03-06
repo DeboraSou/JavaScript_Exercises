a2c8d9c24452b10848fd14bf481a821403e320f9
DeboraSou <debora.d3v@gmail.com>
2024-03-06 00:15:20 -0300

    Declarar variáveis idade e anoNascimento e imprimir a diferença entre os anos.

---
+++ Exer5/script.js
@@ -0,0 +1,7 @@
+var idade = 25;
+let anoNascimento = 1997;
+let anoAtual = 2024;
+
+let diferencaIdade = anoAtual - anoNascimento;
+
+console.log(` O indivíduo nascido em ${anoNascimento}, não tem mais ${idade} anos de idade, com base no ano atual que é ${anoAtual}, ele tem ${diferencaIdade} anos de idade.`);
