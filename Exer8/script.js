4b6750210e19c0b38d972a1d530ba761a27d153e
DeboraSou <debora.d3v@gmail.com>
2024-03-07 15:21:56 -0300

    Realizar operações aritméticas entre duas variáveis numéricas diferentes e imprimir os resultados.

---
+++ Exer8/script.js
@@ -0,0 +1,17 @@
+let num1 = 10;
+let num2 = 5;
+
+let soma = num1 + num2;
+console.log(`A soma de ${num1} por ${num2} é: ${soma}.`);
+
+let subtracao = num1 - num2;
+console.log(`A subtração de ${num1} por ${num2} é: ${subtracao}.`);
+
+let multiplicacao = num1 * num2;
+console.log(`A multiplicação de ${num1} por ${num2} é: ${multiplicacao}.`);
+
+let divisao = num1 / num2;
+console.log(`A divisão de ${num1} por ${num2} é: ${divisao}.`);
+
+let restoDivisao = num1 % num2;
+console.log(`O resto da divisão de ${num1} por ${num2} é: ${restoDivisao}.`);
