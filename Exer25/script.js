21184a7bbc03b7b272ab036058202600796fe388
DeboraSou <debora.d3v@gmail.com>
2024-03-21 10:04:30 -0300

    Utilizar o operador de pré-incremento duas vezes consecutivas seguido pelo operador de pós-incremento na variável 'c' e imprimir o valor final no console.

---
+++ Exer25/script.js
@@ -0,0 +1,10 @@
+let c = 9;
+
+console.log(`Valor original de c: ${c}.`); // 9.
+
+++c;
+++c;
+console.log(`Valor de c após ser pré-incrementado duas vezes consecutivas: ${c}`); // 11.
+
+c++;
+console.log(`Valor final de c, após o pós-incremento: ${c}.`); // 12.
