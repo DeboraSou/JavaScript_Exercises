baa909c084e4a90bead3d96b9c06624a03bce6f5
DeboraSou <debora.d3v@gmail.com>
2024-03-31 23:59:21 -0300

    Criar dois arrays, array1 e array2, cada um com alguns elementos, e utilizar o operador spread para criar um novo array chamado arrayUnido contendo todos os elementos de array1 e array2.

---
+++ Exer33/script.js
@@ -0,0 +1,7 @@
+let array1 = [" Max Verstappen", " Logan Sargeant", " Daniel Ricciardo", " Lando Norris", " Pierre Gasly"];
+let array2 = [" Red Bull Racing", " Williams", " AlphaTauri", " McLaren", " Alpine"];
+let arrayUnido = [...array1, ...array2];
+
+console.log(`Itens do array1:${array1}.`);
+console.log(`\nItens do array2:${array2}.`);
+console.log(`\nJunção do arry1 e array2:${arrayUnido}.`);
