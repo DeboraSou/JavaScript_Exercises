de2985a4b665d76d3297a88266b2f5cc62f206ba
DeboraSou <debora.d3v@gmail.com>
2024-04-02 13:29:37 -0300

    Utilizar condicionais para imprimir saudações apropriadas com base no período do dia, representado por uma variável 'hora'.

---
+++ Exer35/script.js
@@ -0,0 +1,13 @@
+let hora = 8;
+
+if ((hora >= 0) && (hora <= 4)) {
+    console.log("Ainda é madrugada, lua prateada!");
+} else if ((hora >= 5) && (hora <= 11)) {
+    console.log("Bom dia, flor do dia!");
+} else if ((hora >= 12) && (hora <= 18)) {
+    console.log("Boa tarde! riso que acalma.");
+} else if ((hora >= 19) && (hora <= 24)) {
+    console.log("Boa noite, lua princesa!");
+} else {
+    console.log("Vá arrumar algo para fazer!")
+};
