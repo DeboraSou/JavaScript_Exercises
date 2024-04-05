77fbf0f9947f4d2d8761061e475927827950ec5b
DeboraSou <debora.d3v@gmail.com>
2024-04-05 00:01:29 -0300

    38. Crie uma variável chamada diaSemana e atribua um número de 1 a 7 representando um dia da semana.

---
+++ Exer38/script.js
@@ -0,0 +1,28 @@
+let diaSemana = 6;
+
+switch (diaSemana) {
+    case 1:
+        console.log("Domingo.");
+        break;
+    case 2:
+        console.log("Segunda-feira.");
+        break;
+    case 3:
+        console.log("Terça-feira.");
+        break;
+    case 4:
+        console.log("Quarta-feira.");
+        break;
+    case 5:
+        console.log("Quinta-feira.");
+        break;
+    case 6:
+        console.log("Sexta-feira.");
+        break;
+    case 7:
+        console.log("Sábado.");
+        break;
+    default:
+        console.log("Não é um dos dias da semana.");
+        break;
+};
