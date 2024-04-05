e2275d63cca2aa40ba1938599722ff42d0fddcb6
DeboraSou <debora.d3v@gmail.com>
2024-04-04 23:51:05 -0300

    Utilizar um condicional if-else para verificar se duas strings são iguais e imprimir mensagens correspondentes.

---
+++ Exer37/script.js
@@ -0,0 +1,8 @@
+let string1 = "abacate";
+let string2 = "mamao";
+
+if (string1 === string2) {
+    console.log("As strings são iguais.");
+} else {
+    console.log("As strings são diferentes.");
+};
