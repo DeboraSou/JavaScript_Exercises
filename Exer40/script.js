81385d3714b85e17a66202c8303b00ffa56ce7b3
DeboraSou <debora.d3v@gmail.com>
2024-04-07 00:03:39 -0300

    Crie uma variável chamada numero e atribua um número de 1 a 5.

---
+++ Exer40/script.js
@@ -0,0 +1,22 @@
+let numero = 4;
+
+switch (numero) {
+    case 1:
+        console.log("Um é o começo de grandes jornadas!");
+        break;
+    case 2:
+        console.log("Dois olhos vendo, duas mãos criando!");
+        break;
+    case 3:
+        console.log("Três é a magia da trindade!");
+        break;
+    case 4:
+        console.log("Quatro estações, quatro direções!");
+        break;
+    case 5:
+        console.log("Cinco sentidos, cinco estrelas para você!");
+        break;
+    default:
+        console.log("Número fora do intervalo de 1 a 5.");
+        break;
+};
