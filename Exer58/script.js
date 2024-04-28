6b2b293f5248514d36f5844106c9966993193bd5
DeboraSou <debora.d3v@gmail.com>
2024-04-28 00:27:01 -0300

    feat: Adiciona função para verificar se o número é par ou ímpar.

---
+++ Exer58/script.js
@@ -0,0 +1,12 @@
+function verificarParImpar(numero) {
+    if (numero % 2 === 0) {
+        console.log(`O número ${numero}, é Par!`);
+    } else {
+        console.log(`O número ${numero}, é Ímpar!`);
+    }
+}
+
+verificarParImpar(5);
+verificarParImpar(18);
+verificarParImpar(27);
+verificarParImpar(36);
