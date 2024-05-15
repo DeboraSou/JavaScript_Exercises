db52253f65bcec5707936edb96a23e5cbcc489e2
DeboraSou <debora.d3v@gmail.com>
2024-05-15 00:03:23 -0300

    feat: Adiciona a função que atribui parâmetros às propriedades do objeto.

---
+++ Exer71/script.js
@@ -0,0 +1,10 @@
+function Pessoa(nome, idade) {
+    this.nome = nome;
+    this.idade = idade;
+};
+
+let pessoa1 = new Pessoa("Sarah", 36);
+console.log(pessoa1);
+
+let pessoa2 = new Pessoa("Matheus", 25);
+console.log(pessoa2);
